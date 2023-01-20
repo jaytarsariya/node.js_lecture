41ec253986786f41f7b74a81752786ac172a23c6
__JAY TARSARIYA __ <122853471+jaytarsariya@users.noreply.github.com>
2023-01-20 11:50:44 +0530

    Add files via upload

---
+++ module_4.1_c++_basic/2_what_isoop_and_its_concepts.cpp
@@ -0,0 +1,38 @@
+/*
+
+    what is oop?
+    ________________
+    oop is an object orianted program.
+
+    c++ is devleped by bjerne stroutup at at & t's bell leboratory in 1979.
+
+ =>    procedural programming is about writing procedures or that    perform oprations on the data, while object oriented programming is about creating objects that contain both data and functions.
+
+    object-oriented programming has several advantages over procedural programming:
+
+    1. oop is faster and easier to exwcute.
+    2. oop provides a clear structure for the programs.
+    3. oop helps to keep the c++ code   dry "don't repeat yourself", and mack the code easier to maintain, modyfy and debug.
+    4. oop makes it possible ti creat full reusable application with less code and shorter development time.
+
+    ***********************************************************************
+
+                        <:  list oop concepts :>
+
+
+               before starting to learn c++ it is essential to have a basic knowledge of the concepts of object oriented programming.
+
+               some of the important object oriented features are namely:
+
+               1.   objects
+               2.   classes
+               3.   inheritance
+               4.   data abstraction
+               5.   data encapsulation
+               6.   polymorphysm
+               7.   overloading
+               8.   reusability
+
+
+
+*/
