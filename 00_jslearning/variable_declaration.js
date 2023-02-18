9b12b3fc5ff6ff81b9704a61931bdb6abddad85e
__JAY TARSARIYA __ <122853471+jaytarsariya@users.noreply.github.com>
2023-02-18 11:42:30 +0530

    Add files via upload

---
+++ 00_jslearning/variable_declaration.js
@@ -0,0 +1,20 @@
+// var a = 10;
+// var b = 50;
+
+// console.log(a+b);
+
+
+
+ // VARIABLE                                 // DATATYPE
+
+   var a = 10;                                     //number
+   var b = 10.25;                                 //number
+   var c = 'a';                                  //string
+   var d = 'jay';                               //string
+   var e = [10, 20, 30, 40, 'tarsariya']       // object
+   var f;                                     //undefine
+   var g= null;                              //object
+   var h = true;                            //boolean
+
+
+   console.log(a,b,c,d,e,f,g,h,);
