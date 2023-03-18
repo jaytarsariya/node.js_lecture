935c1c7e3e70fd629a843c26019f8fac79555f0a
__JAY TARSARIYA __ <122853471+jaytarsariya@users.noreply.github.com>
2023-03-18 12:18:01 +0530

    Delete Assignment C programing directory

--- Assignment C programing/Assignment_3.1/1_my_informatin/yourinformation.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    printf("my name is jay\n");
-    printf("my birth date 30/05/2002\n");
-    printf("my age is 20\n");
-    printf("my address A/36 balji  nagar soc. dabholi road katargam surat\n");
-    return 0;
-}
--- Assignment C programing/assignment_3.2/assignment_of_oop_26-12/1_hello_world.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "welcom to my program";
-    return 0;
-}
